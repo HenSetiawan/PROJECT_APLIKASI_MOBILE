23eb5278cac399e4fbc814c2bfdaa332c086889c
henSetiawan <hendystyawan74@gmail.com>
2021-07-14 15:04:58 +0700

    add form validation on login user

--- PMI-BON/src/views/Events.vue
+++ PMI-BON/src/views/Events.vue
@@ -1,12 +1,37 @@
 <template>
-  <ion-page></ion-page>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Agenda</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-list>
+        <ion-item> </ion-item>
+      </ion-list>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script>
-import { IonPage } from "@ionic/vue";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+} from "@ionic/vue";
 export default {
   components: {
     IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonItem,
   },
 };
 </script>
--- PMI-BON/src/views/LoginPage.vue
+++ PMI-BON/src/views/LoginPage.vue
@@ -7,30 +7,57 @@
     </ion-header>
     <ion-content>
       <img src="../image/blood.jpg" alt="" />
-      <ion-grid>
-        <ion-row>
-          <p>Username</p>
-        </ion-row>
-        <ion-row class="input-login">
-          <ion-input placeholder="Email" type="text"></ion-input>
-          <ion-icon :icon="person" />
-        </ion-row>
-        <ion-row>
-          <p>Username</p>
-        </ion-row>
-        <ion-row class="input-login">
-          <ion-input placeholder="Password" type="password"></ion-input>
-          <ion-icon :icon="key" />
-        </ion-row>
-        <ion-row>
-          <ion-button color="danger" class="btn-login">Login</ion-button>
-        </ion-row>
-        <ion-row>
-          <ion-button color="primary" class="btn-login" href="/tabs/register"
-            >Register</ion-button
-          >
-        </ion-row>
-      </ion-grid>
+      <Form @submit="onSubmit">
+        <ion-grid>
+          <ion-row>
+            <p>Username</p>
+          </ion-row>
+          <ion-row class="input-login">
+            <Field
+              name="username"
+              as="input"
+              placeholder="username"
+              :rules="isRequired"
+              autocomplete="off"
+            >
+            </Field>
+            <ion-icon :icon="person" />
+          </ion-row>
+          <ion-row class="error-message">
+            <error-message name="username"></error-message>
+          </ion-row>
+
+          <ion-row>
+            <p>Password</p>
+          </ion-row>
+          <ion-row class="input-login">
+            <Field
+              name="password"
+              as="input"
+              placeholder="password"
+              :rules="isRequired"
+              autocomplete="off"
+              type="password"
+            >
+            </Field>
+            <ion-icon :icon="key" />
+          </ion-row>
+          <ion-row class="error-message">
+            <error-message name="password"></error-message>
+          </ion-row>
+
+          <ion-row>
+            <ion-button type="submit" color="danger" class="btn-login"
+              >Login</ion-button
+            >
+          </ion-row>
+          <ion-row>
+            <ion-button color="primary" class="btn-login" href="/tabs/register"
+              >Registrasi</ion-button
+            >
+          </ion-row>
+        </ion-grid>
+      </Form>
     </ion-content>
   </ion-page>
 </template>
@@ -42,12 +69,12 @@ import {
   IonTitle,
   IonToolbar,
   IonContent,
-  IonInput,
   IonGrid,
   IonRow,
   IonIcon,
   IonButton,
 } from "@ionic/vue";
+import { Field, ErrorMessage, Form } from "vee-validate";
 
 import { person, key } from "ionicons/icons";
 export default {
@@ -58,11 +85,13 @@ export default {
     IonTitle,
     IonToolbar,
     IonContent,
-    IonInput,
     IonGrid,
     IonRow,
     IonIcon,
     IonButton,
+    Form,
+    Field,
+    ErrorMessage,
   },
   data() {
     return {
@@ -70,13 +99,26 @@ export default {
       key,
     };
   },
+  methods: {
+    onSubmit(values) {
+      console.log(values);
+      const data = values;
+      console.log(data);
+    },
+    isRequired(value) {
+      if (!value) {
+        return "This field is required";
+      }
+      return true;
+    },
+  },
 };
 </script>
 
 <style scoped>
 p {
   font-size: 12px !important;
-  margin-left: 21px;
+  margin-left: 26px;
 }
 .input-login {
   border-bottom: 1px solid rgb(109, 106, 106);
@@ -91,5 +133,29 @@ img {
   height: 40%;
   margin: auto;
   display: block;
+}
+.input-login {
+  border-bottom: 1px solid rgb(109, 106, 106);
+  margin: auto !important;
+  width: 90% !important;
+}
+input {
+  border: none;
+  outline: none;
+  width: 90%;
+  padding: 8px !important;
+}
+
+input:focus {
+  border: none;
+  outline: none;
+}
+
+.error-message {
+  font-size: 12px !important;
+  margin-left: 21px;
+  color: red !important;
+  font-style: italic;
+  margin-top: 2px !important;
 }
 </style>
